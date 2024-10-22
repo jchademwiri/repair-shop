a97a5dd0adbc1ad2b2b30793ed6c5904c3c28f48
Jacob Chademwiri <jchademwiri@gmail.com>
2024-10-22 05:17:16 +0200

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section>
-      <Button>New Button</Button>
-    </section>
+    <div className="bg-background bg-home-img bg-cover bg-center">
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh ">
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-foreground/90 text-background w-4/5 sm:max-w-96 mx-auto sm:text-2xl">
+          <h1 className="text-3xl font-bold sm:text-4xl" >
+            Jack&apos;s Computer <br /> Repair shop
+          </h1>
+          <address>
+            555 Gateway Lane <br /> Kansas City, KS 5555{" "}
+          </address>
+          <p>Open Daily: 0900 to 1700</p>
+
+          <Link href="tel:000000000" className="hover:underline">
+            000000000
+          </Link>
+        </div>
+      </main>
+    </div>
   );
 }
