5caaa0004da0f520620a98873781fb7cb4d08289
Jacob Chademwiri <jchademwiri@gmail.com>
2024-10-23 11:58:38 +0200

    Handled errors

---
+++ src/app/error.tsx
@@ -0,0 +1,35 @@
+"use client"; // Error boundaries must be Client Components
+
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import * as Sentry from "@sentry/nextjs";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
+
+// continue ... https://youtu.be/djDgTYrFMAY?list=PL0Zuz27SZ-6PCLz7VMP2QQdeKa83rshe5&t=4623
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,8 +3,15 @@
 import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
-export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
@@ -12,6 +19,9 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
   return (
     <html>
       <body>
+        <h2>Something went wrong!</h2>
+
+        <Button onClick={() => reset()}>Try again</Button>
         {/* `NextError` is the default Next.js error page component. Its type
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
