bed0b01e474cfc6f3468f605cf0459520e92c432
Jacob Chademwiri <jchademwiri@gmail.com>
2025-01-01 05:21:52 +0200

    Add TicketSearch component and implement ticket search functionality

--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,8 +1,33 @@
+import TicketSearch from "@/app/(rs)/tickets/TicketSearch";
+import { getOpenTickets } from "@/lib/queries/getOpenTickets";
+import { getTicketSearchResults } from "@/lib/queries/getTicketSearchResults";
+
 export const metadata = {
-  title: 'Tickets',
+  title: "Tickets Search",
 };
 
-const Tickets = () => {
-  return <div>Tickets Page</div>;
-};
-export default Tickets;
+export default async function Tickets({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+  if (!searchText) {
+    const results = await getOpenTickets();
+
+    return (
+      <>
+        <TicketSearch />
+        <p>{JSON.stringify(results)}</p>
+      </>
+    );
+  }
+  const results = await getTicketSearchResults(searchText);
+
+  return (
+    <>
+      <TicketSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
+}
